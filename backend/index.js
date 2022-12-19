41995adc703e15919e0b6adbbd408843c49f4354
Aayush Dongol <ayussh222dongo@gmail.com>
2022-12-19 17:15:27 +0545

    ui completed of messenger

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,14 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import userRoutes from "./backend/routes/userRoutes.js";
 import chatRoutes from "./backend/routes/chatRoutes.js";
-import messageRoutes from "./backend/routes/messageRoutes.js"
-
+import messageRoutes from "./backend/routes/messageRoutes.js";
+import { createRequire } from "module";
 import {
   errorhandler,
   notFound,
 } from "./backend/middleware/errorMiddleware.js";
 
+const require = createRequire(import.meta.url);
 dotenv.config();
 
 connectDB();
@@ -30,4 +31,39 @@ app.use(notFound);
 app.use(errorhandler);
 
 const PORT = process.env.PORT;
-app.listen(PORT, console.log(`Server Running on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server Running on port ${PORT}`));
+
+//connection establishment
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("connected to socket io");
+  socket.on("setup", (userData) => {
+    socket.join(userData._id); //created a uniqe room
+    socket.emit("conencted");
+  });
+
+  socket.on('join chat' , (room) => {
+    socket.join(room); //user will join the chat rrom 
+    console.log(`${room} was joined`)
+  })
+
+socket.on('typing',(room) => socket.in(room).emit("typing"))
+socket.on('stop typing',(room) => socket.in(room).emit("stop typing"))
+
+  socket.on('new message' , (newMessageReceived) => {
+   var chat = newMessageReceived.chat
+
+   if(!chat.users) return console.log("chat users not defined")
+
+   chat.users.forEach(user => {
+    if(user._id === newMessageReceived.sender._id) return;
+    socket.in(user._id).emit("message received",newMessageReceived)
+   });
+  })
+});
